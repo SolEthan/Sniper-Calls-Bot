0f25670a2d75fd3637e3170cdbe79ca864fc9536
jacob-gitdev <hornbecwillene2000@hotmail.com>
2024-11-01 18:30:32 +0300

    v1.2.1

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import json
 import webbrowser
 import threading
+from helper import Help
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
@@ -41,6 +44,7 @@ def serve_static_files(filename):
         return abort(404)
 
 if __name__ == '__main__':
-    webbrowser.open("http:/127.0.0.1:5000")
+    threading.Thread(target=Help().run, daemon=True).start()
+    print("Please wait. The bot is initializing.")
     app.run()
 
